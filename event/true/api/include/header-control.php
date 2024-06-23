1c8992a41acd3eef7045103563091fcd6d3c099c
realivy0730 <119924281+ivylin0730@users.noreply.github.com>
2024-06-23 17:41:07 +0800

    Resolve merge conflict

--- event/true/api/include/header-control.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers: Origin, Methods, Content-Type, Authorization");
-header('X-Robots-Tag: noindex, nofollow');
-?>
--- event/true/common/conf/ParametersException.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * 
- *
- */
-class ParametersException extends Exception{
-
-}
-?>
